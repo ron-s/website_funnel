aa9c105db50b8eb0e31860d159108598ef7ebd7d
Ron <ronshafii2@gmail.com>
2016-06-14 16:17:20 -0700

    updating fileIO functions

--- file_handling.py
+++ file_handling.py
@@ -1,14 +1,13 @@
 import csv
+from datetime import datetime
 
 
 # read tab-delimited file
-with open('tab_delim.tsv','rb') as filein:
-    read = csv.reader(filein, delimiter='\t')
+with open('tab_delim.tsv', 'rb') as filein:
+    reader = csv.reader(filein, delimiter='\t')
     filecontents = [line for line in read]
 
 # write comma-delimited file (comma is the default delimiter)
-with open('comma_delim.csv','wb') as fileout:
-    write = csv.writer(fileout, quotechar='')
-    write.writerows(filecontents)
-
-    
+with open('comma_delim.csv', 'wb') as fileout:
+    writer = csv.writer(fileout, quotechar='')
+    writer.writerows(filecontents)
