861681bef0c3229e13599ed10abfa47580c42f0b
Ron <ronshafii2@gmail.com>
2016-06-15 15:09:09 -0700

    changing strings into json objects

--- file_handling.py
+++ file_handling.py
@@ -1,13 +1,39 @@
 import csv
 from datetime import datetime
+import json
 
 
-# read tab-delimited file
-with open('tab_delim.tsv', 'rb') as filein:
-    reader = csv.reader(filein, delimiter='\t')
-    filecontents = [line for line in read]
 
-# write comma-delimited file (comma is the default delimiter)
-with open('comma_delim.csv', 'wb') as fileout:
-    writer = csv.writer(fileout, quotechar='')
-    writer.writerows(filecontents)
+read tab-delimited file
+def reader():
+	with open('tab_delim.tsv', 'r') as filein:
+		reader = csv.reader(filein, delimiter='\t')
+		yield from reader #generator for low memory usage
+
+
+
+def transform_date(date_str):
+	# do operation
+	date_str = '1/1/2012 5:21:30 AM'
+	datetime_object = datetime.strptime("date_str", "%m/%d/%Y %I/%M/%S/")
+
+	return datetime_object
+
+
+
+for contents in reader():
+	date, name, webpage = contents
+	date = transform_date(date)
+	
+
+	pass
+
+
+
+
+
+# # write comma-delimited file (comma is the default delimiter)
+# with open('comma_delim.csv', 'w') as fileout:
+# 	writer = csv.writer(fileout)
+# 	for contents in reader():
+# 		writer.writerow(contents)
