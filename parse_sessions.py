54ace32c4e190699dfbb472c36eb52bcab811873
Ron <ronshafii2@gmail.com>
2016-06-23 13:07:44 -0700

    taking different appraoch

--- parse_sessions.py
+++ parse_sessions.py
@@ -2,55 +2,74 @@ import csv
 from datetime import datetime
 
 
-class Row:
-    """create a class from the properties of a row in a tab delimited log file"""
-
-    def __init__(self):
-        user = "user"
-        date = "date"
-        url = "url"
 
 
-def reader(filename):
-    """open and read a tabbed delimited file"""
-    #read tab-delimited file
-    with open(filename, 'r') as filein:
-        reader = csv.reader(filein, delimiter='\t')
-        #generator for low memory usage
-        yield from reader
+rows = []
+
+with open("./filein.txt", 'r') as filein:
+    for line in filein:
+        #print(line)
+        #remove the new line character
+        line = line[:-1]
+        #separate the tabbed values in the row
+        date, user, url = line.split("\t")
+
+        rows.append({'date': date, 'user': user, 'url': url})
 
 
-data_dict = {}
 
-R = [reader(filename)]
-
-for i in R:
-    #iterate through the tabbed delimited file to sort the objects by user and add them to a dict
-    if not in data_dict:
-        #if the user doesn't exist in the dict
-        data_dict[i.user] = i
-
-    else:
-        # if user already exists in the dict
-        data_dict[i.user].append = i
+print(rows)
 
 
-newlst = set()
 
-for k, v in data_dict.items():
 
-    x = 0
 
-    for i in range(len(v) - 1):
-        time = i[x].date - i[x]+1.date
+# class Row:
+#     """create a class from the properties of a row in a tab delimited log file"""
 
-        x += 1
+#     def __init__(self):
+#         user = "user"
+#         date = "date"
+#         url = "url"
 
-        if time <= datetime.delta(0, 0, 0, 0, 20):
-        #if the difference between the timestamps is 20min or less then it belongs to the same session
-            newlst.add(i[x])
-            newlst.add(i[x + 1])
 
-        else:
-        #if the difference between the timestamps is greater than 20min then it belongs to a new session
-            break
+# def reader(filename):
+#     """open and read a tabbed delimited file"""
+#     #read tab-delimited file
+#     with open(filename, 'r') as filein:
+#         reader = csv.reader(filein, delimiter='\t')
+#         #generator for low memory usage
+#         yield from reader
+
+
+
+# for i in R:
+#     #iterate through the tabbed delimited file to sort the objects by user and add them to a dict
+#     if not in data_dict:
+#         #if the user doesn't exist in the dict
+#         data_dict[i.user] = i
+
+#     else:
+#         # if user already exists in the dict
+#         data_dict[i.user].append = i
+
+
+# newlst = set()
+
+# for k, v in data_dict.items():
+
+#     x = 0
+
+#     for i in range(len(v) - 1):
+#         time = i[x].date - i[x]+1.date
+
+#         x += 1
+
+#         if time <= datetime.delta(0, 0, 0, 0, 20):
+#         #if the difference between the timestamps is 20min or less then it belongs to the same session
+#             newlst.add(i[x])
+#             newlst.add(i[x + 1])
+
+#         else:
+#         #if the difference between the timestamps is greater than 20min then it belongs to a new session
+#             break
